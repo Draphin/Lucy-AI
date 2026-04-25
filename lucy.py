2008c76571f487dc08379715fff918f27c29906c
Draphin <draphin83@gmail.com>
2026-04-25 11:23:24 -0400

    Update lucy.py

--- lucy.py
+++ lucy.py
@@ -38,7 +38,6 @@ def ask_lucy(prompt, history, facts):
     {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"}
 ]
-        ]
     }
     
     try:
